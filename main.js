dbb9b8712a6cff2e82b332c6daf63702b779c266
wafeliciano20 <firstwillow2@gmail.com>
2021-07-20 12:45:26 -0400

    v2

--- main.js
+++ main.js
@@ -3,29 +3,36 @@ const adjectives = ['angry', 'lazy', 'clumpsy', 'happy', 'eager', 'calm']
 const nouns = ['programmer', 'software engineer', 'developer', 'engineer'];
 const verbs = ['wrote', 'developed', 'tested', 'broke']
 const secondNouns = ['virus', 'program', 'database', 'code base', 'server'];
+let numberOfSentences = 5;
+let mixedMessages = [];
 
+const generateRandomNumber = (length) => {
+    return Math.floor(Math.random() * length);
+}
 
 const generateMessage = () => {
-    let firstDeterminant = determiners[Math.floor(Math.random() * determiners.length)];
-    let adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
-    let noun = nouns[Math.floor(Math.random() * nouns.length)];
-    let verb = verbs[Math.floor(Math.random() * verbs.length)];
-    let secondDeterminant = determiners[Math.floor(Math.random() * determiners.length)];
-    let secondAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
-    let secondNoun = secondNouns[Math.floor(Math.random() * secondNouns.length)];
+    let firstDeterminant = determiners[generateRandomNumber(determiners.length)];
+    let adjective = adjectives[generateRandomNumber(adjectives.length)];
+    let noun = nouns[generateRandomNumber(nouns.length)];
+    let verb = verbs[generateRandomNumber(verbs.length)];
+    let secondDeterminant = determiners[generateRandomNumber(determiners.length)];
+    let secondAdjective = adjectives[generateRandomNumber(adjectives.length)];
+    let secondNoun = secondNouns[generateRandomNumber(secondNouns.length)];
 
     let message = `${firstDeterminant} ${adjective} ${noun} ${verb} ${secondDeterminant} ${secondAdjective} ${secondNoun}.`;
     let firstLetter = message[0];
     message = message.replace(message.charAt(0), firstLetter.toUpperCase());
     return message;
 }
-let numberOfSentences = 5;
-let mixedMessages = [];
 
-console.log('\n\n');
-for (let i = 0; i < numberOfSentences; i++) {
-    mixedMessages.push(generateMessage());
-    console.log(mixedMessages[i]);
-    console.log('\n');
+const printSentences = (numberOfSentences) => {
+    console.log('\n\n');
+    for (let i = 0; i < numberOfSentences; i++) {
+        mixedMessages.push(generateMessage());
+        console.log(mixedMessages[i]);
+        console.log('\n');
+    }
+    console.log('\n\n');
 }
-console.log('\n\n');
+
+printSentences(numberOfSentences);
